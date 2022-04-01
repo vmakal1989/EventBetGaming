1e825427a8412fd6f2396e84cfc5d11a30945ab4
Makal Vitali <Makal.V@digdes.com>
2022-04-01 13:55:05 +0300

    added sass, vuex, axios, vue-router, lodash, bus

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import lodash from 'lodash'
+import VueBus from 'vue-bus'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$_ = lodash
+Vue.use(VueBus)
+
 
 new Vue({
   render: h => h(App),
